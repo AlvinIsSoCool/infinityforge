4578696da278b474daccaf6540eedb5cd9cdc109
AlvinIsSoCool <lindageorge5335@gmail.com>
2026-04-09 13:52:21 +0530

    Initial Commit

---
+++ src/main/java/net/alvin/infinityforge/InfinityForge.java
@@ -0,0 +1,16 @@
+package net.alvin.infinityforge;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class InfinityForge implements ModInitializer {
+	public static final String MOD_ID = "infinityforge";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/alvin/infinityforge/InfinityForgeDataGenerator.java
@@ -0,0 +1,11 @@
+package net.alvin.infinityforge;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class InfinityForgeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
