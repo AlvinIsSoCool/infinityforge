878118f4fa27934610ff407927bc4653aa2940a5
AlvinIsSoCool <lindageorge5335@gmail.com>
2026-09-06 12:41:31 +0530

    1.0.0 Release

--- src/main/java/net/alvin/infinityforge/client/event/GauntletHudEvents.java
+++ src/main/java/net/alvin/infinityforge/client/event/GauntletHudEvents.java
@@ -1,13 +1,19 @@
 package net.alvin.infinityforge.client.event;
 
 import net.alvin.infinityforge.client.hud.GauntletHudRenderer;
+import net.alvin.infinityforge.client.hud.TestRendererPassives;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 
 public class GauntletHudEvents {
+    private static final boolean DEBUG = false;
+
     public static void register() {
         HudRenderCallback.EVENT.register((context, tickDelta) ->
                 GauntletHudRenderer.render(context));
-        //HudRenderCallback.EVENT.register((context, tickDelta) ->
-                //TestRendererPassives.render(context));
+
+        if (DEBUG) {
+            HudRenderCallback.EVENT.register((context, tickDelta) ->
+                    TestRendererPassives.render(context));
+        }
     }
 }
